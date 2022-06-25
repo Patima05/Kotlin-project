1812867e1a95d3ca37691d58c3baff9626957f48
Patima05 <Patimat777@gmail.com>
2022-06-25 15:42:03 +0300

    Урок 1 | Создан Object, вызван copy

--- app/src/main/java/com/example/kotlin_project/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_project/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.kotlin_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var testButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +25,15 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(
                 this,
                 R.string.test_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        val copyNote = testNote.copy()
+        findViewById<Button>(R.id.copy_button).setOnClickListener{ view: View ->
+            Toast.makeText(
+                this,
+                copyNote.toString(),
                 Toast.LENGTH_SHORT)
                 .show()
         }
